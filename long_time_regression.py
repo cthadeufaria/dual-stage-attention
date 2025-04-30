db6ab89da8c563da4926fcdf1bd2d35660c4737e
cthadeufaria <crls.thadeu@gmail.com>
2025-04-30 16:56:59 +0100

    masks update

--- long_time_regression.py
+++ long_time_regression.py
@@ -1,5 +1,6 @@
+import torch
 import torch.nn as nn
-from torch import randn, triu, ones
+from torch import randn
 from torch.nn import TransformerEncoderLayer, TransformerEncoder
 
 
@@ -8,19 +9,22 @@ class LongTimeRegression(nn.Module):
     Implements the long-time regression network for the dual-stage attention model.
     Multi-Head Masked Attention implementation described @ 
     https://www.geeksforgeeks.org/how-to-use-pytorchs-nnmultiheadattention/#handling-masks-with-nnmultiheadattention.
-    src_key_padding_mask implementation described @ https://iori-yamahata.net/2024/02/28/programming-1-eng/.
+    
+    src_key_padding_mask implementation described @ https://iori-yamahata.net/2024/02/28/programming-1-eng/
+    and @ https://sanjayasubedi.com.np/deeplearning/masking-in-attention/.
     """
-    def __init__(self, num_layers=1, max_seq_len=1000, model_dim=180, n_head=4):
+    def __init__(self, padding, num_layers, model_dim=180, n_head=4):
         super(LongTimeRegression, self).__init__()
 
-        self.position_encoder = nn.Parameter(randn(max_seq_len, model_dim))
+        self.position_encoder = nn.Parameter(randn(padding, model_dim))
 
         encoder_layer = TransformerEncoderLayer(
-            model_dim, 
-            n_head, 
-            dim_feedforward=4 * model_dim, 
+            model_dim,
+            n_head,
+            dim_feedforward=4 * model_dim,
             dropout=0.1,
-            activation='relu'
+            activation='relu',
+            batch_first=True,
         )
         self.transformer_encoder = TransformerEncoder(encoder_layer, num_layers=num_layers)
 
@@ -29,23 +33,20 @@ class LongTimeRegression(nn.Module):
 
     def forward(self, x):
         """
-        Input shape: (batch_size, seq_len, d_model)
-        Output shape: (batch_size, seq_len, d_model)
+        Input shape: (seq_len, d_model)
+        Output shape: (seq_len, d_model)
         """
-        x = x[None, None, :] if len(x.shape) == 1 else x[None, :] if len(x.shape) == 2 else x
-        seq_len = x.shape[1] # T video chunk
-        # TODO: create src_key_padding_mask
-        src_key_padding_mask = None
+        seq_len = x.shape[0]
 
-        F = x + self.position_encoder[None, :seq_len, :]  # (1, seq_len, d_model)
-        F = F.permute(1, 0, 2) # (seq_len, 1, d_model)
+        F = x + self.position_encoder[:seq_len, :]
 
-        causal_mask = nn.Transformer.generate_square_subsequent_mask(seq_len).to(F.device) 
+        causal_mask = nn.Transformer.generate_square_subsequent_mask(seq_len).to(F.device)
 
-        C = self.transformer_encoder(F, mask=causal_mask, src_key_padding_mask=src_key_padding_mask)
-        # C = self.transformer_encoder(F, mask=causal_mask)
-        C = C.permute(1, 0, 2) # Return to original shape (1, seq_len, d_model)
+        C = self.transformer_encoder(
+            F,
+            mask=causal_mask,
+        )
 
-        O = self.activation(self.FC(C)) + C  # TODO: this should be the output of each encoder block. And there's many encoder blocks supposedly. Double check theory.
+        O = self.activation(self.FC(C)) + C
 
-        return O.squeeze(0)
+        return O
