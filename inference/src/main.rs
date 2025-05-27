cd6d8536a9154e33a01e9dad5dff9c3339e0cddc
cthadeufaria <crls.thadeu@gmail.com>
2025-05-27 18:57:36 +0100

    Rust inference

--- inference/src/main.rs
+++ inference/src/main.rs
@@ -1,18 +1,34 @@
-use tch::{CModule, Tensor};
+use tch::{CModule, Device, Tensor, IValue};
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Load a TorchScript model and perform inference using the `tch` crate in Rust.
-    // More information available @:
-    // https://github.com/LaurentMazare/tch-rs/blob/main/examples/jit/README.md
-    // https://blog.gopenai.com/migrating-trained-pytorch-model-to-rust-a51869e8a51c
-    // https://medium.com/@heyamit10/loading-and-running-a-pytorch-model-in-rust-f10d2577d570
+    let model: CModule = CModule::load_on_device(
+        "/home/dev/repos/dual-stage-attention/runs/models/torchscript/DUAL_ATTENTION_LIVENFLX_II_2025-05-16_17:43:05_EPOCH_23.pt", 
+        Device::Cpu
+    )?;
 
-    let model: CModule = CModule::load("/home/dev/repos/dual-stage-attention/runs/models/torchscript/DUAL_ATTENTION_LIVENFLX_II_2025-05-16_17:43:05_EPOCH_23.pt")?;
+    // Create each tensor with the required shapes
+    let tensor1 = Tensor::randn(&[3, 216, 224, 224], (tch::Kind::Float, Device::Cpu));
+    let tensor2 = Tensor::randn(&[3, 864, 224, 224], (tch::Kind::Float, Device::Cpu));
+    let tensor3 = Tensor::randn(&[3, 27, 1080, 1920], (tch::Kind::Float, Device::Cpu));
+    let tensor4 = Tensor::randn(&[27, 4], (tch::Kind::Float, Device::Cpu));
 
-    let input: Tensor = Tensor::randn(&[1, 3, 224, 224], (tch::Kind::Float, tch::Device::Cpu));
+    // Build the nested tuple structure
+    let input = IValue::Tuple(vec![
+        IValue::Tuple(vec![
+            IValue::Tuple(vec![
+                IValue::Tensor(tensor1),
+                IValue::Tensor(tensor2)
+            ]),
+            IValue::Tensor(tensor3)
+        ]),
+        IValue::Tensor(tensor4)
+    ]);
 
-    let output: Tensor = model.forward_ts(&[input])?;
+    println!("Inference input: {:?}", input);
+
+    let output: IValue = model.forward_is(&[input])?;
 
     println!("Inference output: {:?}", output);
 
