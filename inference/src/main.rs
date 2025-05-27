86319493fd0c2000ea3b3cb48f0faa15f75ab1bd
cthadeufaria <crls.thadeu@gmail.com>
2025-05-27 20:59:37 +0100

    Rust tch-rs Cuda support added

--- inference/src/main.rs
+++ inference/src/main.rs
@@ -1,18 +1,37 @@
-use tch::{CModule, Device, Tensor, IValue};
-
+use tch::{
+    CModule, 
+    Device, 
+    Tensor, 
+    IValue, 
+    nn::VarStore
+};
+use std::ffi::CString;
+use libc::dlopen;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Load a TorchScript model and perform inference using the `tch` crate in Rust.
+    // Solution to successfully use Cuda @ https://www.reddit.com/r/rust/comments/1j8vbww/torch_tchrs_with_cuda/.
+
+    let path = CString::new("/home/dev/repos/dual-stage-attention/.venv/lib/python3.10/site-packages/torch/lib/libtorch_cuda.so").unwrap();
+    unsafe {
+        dlopen(path.into_raw(), 1);
+    }
+
+    let vs = VarStore::new(Device::cuda_if_available());
+    println!("CUDA available: {}", tch::Cuda::is_available());
+    println!("CUDA device count: {}", tch::Cuda::device_count());
+    println!("Using device: {:?}", vs.device());
+    
     let model: CModule = CModule::load_on_device(
         "/home/dev/repos/dual-stage-attention/runs/models/torchscript/DUAL_ATTENTION_LIVENFLX_II_2025-05-16_17:43:05_EPOCH_23.pt", 
-        Device::Cpu
+        vs.device()
     )?;
 
     // Create each tensor with the required shapes
-    let tensor1 = Tensor::randn(&[3, 216, 224, 224], (tch::Kind::Float, Device::Cpu));
-    let tensor2 = Tensor::randn(&[3, 864, 224, 224], (tch::Kind::Float, Device::Cpu));
-    let tensor3 = Tensor::randn(&[3, 27, 1080, 1920], (tch::Kind::Float, Device::Cpu));
-    let tensor4 = Tensor::randn(&[27, 4], (tch::Kind::Float, Device::Cpu));
+    let tensor1 = Tensor::randn(&[3, 216, 224, 224], (tch::Kind::Float, vs.device()));
+    let tensor2 = Tensor::randn(&[3, 864, 224, 224], (tch::Kind::Float, vs.device()));
+    let tensor3 = Tensor::randn(&[3, 27, 1080, 1920], (tch::Kind::Float, vs.device()));
+    let tensor4 = Tensor::randn(&[27, 4], (tch::Kind::Float, vs.device()));
 
     // Build the nested tuple structure
     let input = IValue::Tuple(vec![
